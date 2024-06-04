ce1ffc3da94a58e28879b0f4fc6cf15eeb159660
Your Name <you@example.com>
2024-06-04 18:38:40 +0100

    ùain

---
+++ tailwind.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          grayColor: '#FCF9F7',
+          primaryColor: '#C6754D',
+          secondaryColor: '#2B1105',
+        },
+
+        fontSize: {
+          '2xl': '20px',
+          '2.5xl': '24px',
+          '3xl': '26px',
+          '5xl': '60px',
+          '8xl': '101px',
+        },
+
+        fontFamily: {
+          'montaga': 'Montaga', 
+        },
+      },
+    }
+  }
+
